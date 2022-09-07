7bb2bb232da5bc4988c4daf4e09640128be7573d
Iris Izydorczak <iizydorc@hamilton.edu>
2022-09-07 16:45:48 -0400

    original code

---
+++ project0.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+std::string code[] = {
+",","#include <iostream>","std::string code[] = {","int main(){","   for (int i = 1; i < 3; i++){","      std::cout << code[i]<< std::endl;","   }","   for (int i = 0; i < 17; i++){","       std::cout << char(34) << code[i] << char(34) << code[0];","   }","   std::cout << code[16] <<std::endl;","   for (int i = 3; i < 16; i++){","       std::cout << code[i]<< std::endl;","   }","   return 0;","}","};",
+};
+int main(){
+   for (int i = 1; i < 3; i++){
+      std::cout << code[i]<< std::endl;
+   }
+   for (int i = 0; i < 17; i++){
+       std::cout << char(34) << code[i] << char(34) << code[0];
+   }
+   std::cout << code[16] <<std::endl;
+   for (int i = 3; i < 16; i++){
+       std::cout << code[i]<< std::endl;
+   }
+   return 0;
+}
